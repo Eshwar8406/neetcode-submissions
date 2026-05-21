052146eb4b80078451da41f681335fed4b92effd
Padishala Eshwar <padishala.eshwar@gmail.com>
2026-05-22 00:17:05 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        boolean ans=false;
+        if(s.length()==t.length()){
+            char[] sc=s.toCharArray();
+            Arrays.sort(sc);
+            String ss=new String(sc);
+            char[] tc=t.toCharArray();
+            Arrays.sort(tc);
+            String st=new String(tc);
+            if(ss.equals(st)){
+                ans=true;
+            }
+        }
+        return ans;
+    }
+}
